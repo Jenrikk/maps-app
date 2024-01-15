b8316fd4fede31ce32191915ceec9aecca596862
quique <henrix427@gmail.com>
2024-01-15 23:14:55 +0100

    add SearchBar and SearchResults components

---
+++ src/components/search-results/SearchResults.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="list-group mt-3">
+    <li class="list-group-item list-group-item-action">
+      <h5>Place name</h5>
+      <div align="right">
+        <button class="btn btn-outline-primary">Directions</button>
+      </div>
+    </li>
+    <li class="list-group-item list-group-item-action">
+      <h5>Place name</h5>
+      <div align="right">
+        <button class="btn btn-outline-primary">Directions</button>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SearchResults",
+  setup() {
+    return {};
+  },
+});
+</script>
+
+
+<style scoped>
+li{
+    cursor: pointer;
+}
+</style>
---
+++ src/components/searchbar/SearchBar.vue
@@ -0,0 +1,26 @@
+<script lang="ts" src="./SearchBar.ts" />
+
+<template>
+  <div class="searchbar-container">
+    <input type="text" class="form-control" placeholder="Madrid...">
+    <SearchResults />
+    
+  </div>
+</template>
+
+<style scoped>
+.searchbar-container {
+    position: fixed; 
+    top: 30px; 
+    left: 30px; 
+    background-color: #fff;
+    z-index: 999;
+    box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.3);
+    width: 230px;
+    border-radius: 10px !important;
+    overflow: hidden;
+    padding: 5px;
+}
+
+
+</style>
