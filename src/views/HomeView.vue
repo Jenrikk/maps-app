d078f5f4c35f5ac02f12dd0526febb9b3f37c240
quique <henrix427@gmail.com>
2024-01-15 20:57:40 +0100

    add my-location button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <template>
   <MapView />
+  <MyLocationBtn />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { usePlacesStore } from '@/composables';
 import MapView from '../components/mapview/MapView.vue'
+import MyLocationBtn from '../components/my-location-btn/MyLocationBtn.vue'
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    MapView
+    MapView,
+    MyLocationBtn
   },
   setup() {
 
